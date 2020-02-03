75d567b384698af9daeb627d25bf2172410476dc
erdaksu <erdemaksu77@gmail.com>
2020-02-03 05:01:47 +0000

    added the images needed and also the courses.js file

---
+++ data/courses.js
@@ -0,0 +1,101 @@
+var courses = [
+	{
+		slug: 'mathshendon',
+		topic: 'Maths Classes',
+		location: 'Hendon',
+		price: '£100',
+	},
+	{
+		slug: 'mathscolindale',
+		topic: 'Maths Classes',
+		location: 'Colindale',
+		price: '£110',
+	},
+	{
+		slug: 'mathsbrentcross',
+		topic: 'Maths Classes',
+		location: 'BrentCross',
+		price: '£90',
+	},
+	{
+		slug: 'mathsgoldersgreen',
+		topic: 'Maths Classes',
+		location: 'GoldersGreen',
+		price: '£80',
+	},
+
+	{
+		slug: 'englishhendon',
+		topic: 'English Classes',
+		location: 'Hendon',
+		price: '£70',
+	},
+	{
+		slug: 'englishcolindale',
+		topic: 'English Classes',
+		location: 'Colindale',
+		price: '£70',
+	},
+	{
+		slug: 'englishbrentcross',
+		topic: 'English Classes',
+		location: 'BrentCross',
+		price: '£90',
+	},
+	{
+		slug: 'englishgoldersgreen',
+		topic: 'English Classes',
+		location: 'GoldersGreen',
+		price: '£80',
+	},
+
+	{
+		slug: 'sciencehendon',
+		topic: 'Science Classes',
+		location: 'Hendon',
+		price: '£90',
+	},
+	{
+		slug: 'sciencecolindale',
+		topic: 'Science Classes',
+		location: 'Colindale',
+		price: '£70',
+	},
+	{
+		slug: 'sciencebrentcross',
+		topic: 'Science Classes',
+		location: 'BrentCross',
+		price: '£70',
+	},
+	{
+		slug: 'sciencegoldersgreen',
+		topic: 'Science Classes',
+		location: 'GoldersGreen',
+		price: '£80',
+	},
+
+	{
+		slug: 'sportshendon',
+		topic: 'Sports Classes',
+		location: 'Hendon',
+		price: '£50',
+	},
+	{
+		slug: 'sportscolindale',
+		topic: 'Sports Classes',
+		location: 'Colindale',
+		price: '£35',
+	},
+	{
+		slug: 'sportsbrentcross',
+		topic: 'Sports Classes',
+		location: 'BrentCross',
+		price: '£40',
+	},
+	{
+		slug: 'sportsgoldersgreen',
+		topic: 'Sports Classes',
+		location: 'GoldersGreen',
+		price: '£50',
+	},
+];
