00f8644b9232694d562ff58ed02c9d1e480ac287
erdaksu <erdemaksu77@gmail.com>
2020-02-05 10:19:28 +0000

    pwa update

--- sw.js
+++ sw.js
@@ -26,10 +26,10 @@ for (var i = 0; i < courses.length; i++) {
 var contentToCache = appShellFiles.concat(coursesImages);
 
 // Installing Service Worker
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
   console.log('[Service Worker] Install');
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open(cacheName).then(function (cache) {
       console.log('[Service Worker] Caching all: app shell and content');
       return cache.addAll(contentToCache);
     })
@@ -37,12 +37,12 @@ self.addEventListener('install', function(e) {
 });
 
 // Fetching content using Service Worker
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function (e) {
   e.respondWith(
-    caches.match(e.request).then(function(r) {
-      console.log('[Service Worker] Fetching resource: '+e.request.url);
-      return r || fetch(e.request).then(function(response) {
-        return caches.open(cacheName).then(function(cache) {
+    caches.match(e.request).then(function (r) {
+      console.log('[Service Worker] Fetching resource: ' + e.request.url);
+      return r || fetch(e.request).then(function (response) {
+        return caches.open(cacheName).then(function (cache) {
           console.log('[Service Worker] Caching new resource: ' + e.request.url);
           cache.put(e.request, response.clone());
           return response;
@@ -52,5 +52,24 @@ self.addEventListener('fetch', function(e) {
   );
 });
 
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('Progressive-Web-App-v2').then((cache) => {
+      return cache.addAll(contentToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
 
 
