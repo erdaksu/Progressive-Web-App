85cabaf9f99053670585fe552fbdb6719aeaeddf
erdaksu <erdemaksu77@gmail.com>
2020-02-05 03:22:43 +0000

    trying to fix error

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const vueapp = new Vue({
 
 // Registering Service Worker
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/Progressive-Web-App/sw.js');
+	navigator.serviceWorker.register('sw.js');
 };
 
 // Requesting permission for Notifications after clicking on the button
