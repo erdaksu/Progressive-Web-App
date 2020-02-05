bbbe75995035f907f7e1e47634f688c0a9a5e266
erdaksu <erdemaksu77@gmail.com>
2020-02-05 00:35:56 +0000

    pwa update

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-var template = "<article>\n\
-	<img src='data/img/placeholder.png' data-src='data/img/SLUG.png' alt='NAME'>\n\
-	<h3>#POS. TOPIC</h3>\n\
-	<span>Topic:</span> <strong>TOPIC</strong>\n\
-	<br>\n\
-	<span>Location:</span> <strong>LOCATION</strong>\n\
-	<br>\n\
-	<span>Price:</span> <strong>PRICE</strong>\n\
-	<br>\n\
-	<span>More:</span> <a href='http://classesandactivities/SLUG'>classesandactivities/SLUG</a>\n\
-</article>";
-var content = '';
-for(var i=0; i<courses.length; i++) {
-	var entry = template.replace(/POS/g,(i+1))
-		.replace(/SLUG/g,courses[i].slug)
-		.replace(/TOPIC/g,courses[i].topic)
-		.replace(/LOCATION/g,courses[i].location)
-		.replace(/PRICE/g,courses[i].price)
-	entry = entry.replace('<a href=\'http:///\'></a>','-');
-	content += entry;
-};
-document.getElementById('content').innerHTML = content;
+const vueapp = new Vue({
+	el: '#content',
+	data: {
+		courses: [
+			{ slug: 'mathshendon', topic: 'Maths Classes', location: 'Hendon', price: '£100', },
+			{ slug: 'mathscolindale', topic: 'Maths Classes', location: 'Colindale', price: '£110', },
+			{ slug: 'mathsbrentcross', topic: 'Maths Classes', location: 'BrentCross', price: '£90', },
+			{ slug: 'mathsgoldersgreen', topic: 'Maths Classes', location: 'GoldersGreen', price: '£80', },
+			{ slug: 'englishhendon', topic: 'English Classes', location: 'Hendon', price: '£70', },
+			{ slug: 'englishcolindale', topic: 'English Classes', location: 'Colindale', price: '£70', },
+			{ slug: 'englishbrentcross', topic: 'English Classes', location: 'BrentCross', price: '£90', },
+			{ slug: 'englishgoldersgreen', topic: 'English Classes', location: 'GoldersGreen', price: '£80', },
+			{ slug: 'sciencehendon', topic: 'Science Classes', location: 'Hendon', price: '£90', },
+			{ slug: 'sciencecolindale', topic: 'Science Classes', location: 'Colindale', price: '£70', },
+			{ slug: 'sciencebrentcross', topic: 'Science Classes', location: 'BrentCross', price: '£70', },
+			{ slug: 'sciencegoldersgreen', topic: 'Science Classes', location: 'GoldersGreen', price: '£80', },
+			{ slug: 'sportshendon', topic: 'Sports Classes', location: 'Hendon', price: '£50', },
+			{ slug: 'sportscolindale', topic: 'Sports Classes', location: 'Colindale', price: '£35', },
+			{ slug: 'sportsbrentcross', topic: 'Sports Classes', location: 'BrentCross', price: '£40', },
+			{ slug: 'sportsgoldersgreen', topic: 'Sports Classes', location: 'GoldersGreen', price: '£50', },]
+	}
+})
 // Registering Service Worker
-if('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('sw.js');
 };
 
 // Requesting permission for Notifications after clicking on the button
 var button = document.getElementById("notifications");
-button.addEventListener('click', function(e) {
-	Notification.requestPermission().then(function(result) {
-		if(result === 'granted') {
+button.addEventListener('click', function (e) {
+	Notification.requestPermission().then(function (result) {
+		if (result === 'granted') {
 			randomNotification();
 		}
 	});
@@ -37,10 +37,10 @@ button.addEventListener('click', function(e) {
 
 // Setting up random Notification
 function randomNotification() {
-	var randomItem = Math.floor(Math.random()*courses.length);
+	var randomItem = Math.floor(Math.random() * courses.length);
 	var notifTitle = courses[randomItem].topic;
-	var notifBody = 'Classes Located: '+courses[randomItem].location+'.';
-	var notifImg = 'data/img/'+courses[randomItem].slug+'.png';
+	var notifBody = 'Classes Located: ' + courses[randomItem].location + '.';
+	var notifImg = 'data/img/' + courses[randomItem].slug + '.png';
 	var options = {
 		body: notifBody,
 		icon: notifImg
@@ -51,27 +51,27 @@ function randomNotification() {
 
 // Progressive loading images
 var imagesToLoad = document.querySelectorAll('img[data-src]');
-var loadImages = function(image) {
+var loadImages = function (image) {
 	image.setAttribute('src', image.getAttribute('data-src'));
-	image.onload = function() {
+	image.onload = function () {
 		image.removeAttribute('data-src');
 	};
 };
-if('IntersectionObserver' in window) {
-	var observer = new IntersectionObserver(function(items, observer) {
-		items.forEach(function(item) {
-			if(item.isIntersecting) {
+if ('IntersectionObserver' in window) {
+	var observer = new IntersectionObserver(function (items, observer) {
+		items.forEach(function (item) {
+			if (item.isIntersecting) {
 				loadImages(item.target);
 				observer.unobserve(item.target);
 			}
 		});
 	});
-	imagesToLoad.forEach(function(img) {
+	imagesToLoad.forEach(function (img) {
 		observer.observe(img);
 	});
 }
 else {
-	imagesToLoad.forEach(function(img) {
+	imagesToLoad.forEach(function (img) {
 		loadImages(img);
 	});
 }
